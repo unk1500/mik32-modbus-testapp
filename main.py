c8c05ef1c6a24a99d130f2d8247d81a89b60aa3f
snsavelev <snsavelev@istokmw.ru>
2026-01-18 18:00:00 +0300

    registers table added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# ver 0.1
+    # ver 0.1
 
 from pymodbus.client import ModbusSerialClient
 from pymodbus import FramerType
 
-controller_device_id=1
+tty_port = "/dev/ttyUSB1"
+controller_device_id = 1
+devices_count = 2
 
 def check_negative_temperature(temperature):
     if temperature & 0x8000:
@@ -11,72 +13,103 @@ def check_negative_temperature(temperature):
     else:
         return (temperature & 0x7FFF) * 0.1
 
-def read_all_registers(client):
-    print(f"{'   | DO: HEATER | DO:  FAN   | DI: REED | AI: OUTDOOR | AI: INDOOR  | AO: FWVER'}")
+def read_device_registers(client, device_id):
+    try:
+        # Connecting to device
+        res = client.read_holding_registers(address=0, count=1, device_id=device_id)
+        # print("Device #{} found!".format(device_id))
+        # Reading Coils
+        coils = dict()
+        res = client.read_coils(address=0, count=2, device_id=device_id)
+        coils["heater"] = "ON" if res.bits[0] else "OFF"
+        coils["fan"] = "ON" if res.bits[2] else "OFF"
+        # print(coils)
+        # Reading Reed
+        # !!!
+        reed = "CLOSE"
+        # Reading Temperature and Humidity
+        temperature = dict()
+        humidity = dict()
+        res = client.read_input_registers(address=0, count=1, device_id=device_id)
+        temperature["outdoor"] = "ERROR" if res.registers[0] == 0xFFFF \
+            else round(check_negative_temperature(res.registers[0]), 1)
+        res = client.read_input_registers(address=1, count=1, device_id=device_id)
+        temperature["indoor"] = "ERROR" if res.registers[0] == 0xFFFF \
+            else round(check_negative_temperature(res.registers[0]), 1)
+        res = client.read_input_registers(address=400, count=1, device_id=device_id)
+        humidity["outdoor"] = "ERROR" if res.registers[0] == 0xFFFF \
+            else (res.registers[0] / 10)
+        res = client.read_input_registers(address=401, count=1, device_id=device_id)
+        humidity["indoor"] = "ERROR" if res.registers[0] == 0xFFFF \
+            else (res.registers[0] / 10)
+        # print(temperature, humidity)
+        # Reading FW Version
+        res = client.read_holding_registers(address=2006, count=2, device_id=device_id)
+        version = "{}{}{}{}".format(
+            chr((res.registers[0] >> 8) & 0xFF),
+            chr(res.registers[0] & 0xFF),
+            chr((res.registers[1] >> 8) & 0xFF),
+            chr(res.registers[1] & 0xFF)
+        )
+        # print(version)
+
+        print(f""
+              f"{device_id:^3}|"
+              f"{coils['heater']:^12}|"
+              f"{coils['fan']:^12}|"
+              f"{reed:^10}|"
+              f"{(str(temperature['outdoor']) + ' °C'):^13}|"
+              f"{(str(temperature['indoor']) + ' °C'):^13}|"
+              f"{version:^10}"
+        )
+        print(f""
+              f"{' ':^3}|"
+              f"{' ':^12}|"
+              f"{' ':^12}|"
+              f"{' ':^10}|"
+              f"{(str(humidity['outdoor']) + ' %'):^13}|"
+              f"{(str(humidity['indoor']) + ' %'):^13}|"
+              f"{' ':^10}"
+              )
+    except BaseException as e:
+        #print(e)
+        print(f"                         Device {device_id} not found                          ")
+
+
+def read_all_devices(client):
+    print(f"   | DO: HEATER | DO:  FAN   | DI: REED | AI: OUTDOOR | AI: INDOOR  | AO: FWVER")
     print("-"*79)
-    print(f"{'00 |     ON     |     ON     |   OPEN   |     28.5 °C |    28.5 °C  |     v0.1 '}")
-    print(f"{'   |            |            |          |     36.0 %  |    36.0 %   |     v0.1 '}")
-    print(f"{'01 |     OFF    |     OFF    |   CLOSE  |     28.5 °C |    28.5 °C  |     v0.1 '}")
-    print(f"{'   |            |            |          |     36.0 %  |    36.0 %   |     v0.1 '}")
-    print()
-
-    print("== DIGITAL OUTPUTS ==")
-    res = client.read_coils(address=0, count=2, device_id=controller_device_id)
-    if res.bits[0]:
-        print("Relay Heater ON")
-    else:
-        print("Relay Heater OFF")
-    if res.bits[1]:
-        print("Relay Fan    ON")
-    else:
-        print("Relay Fan    OFF")
-    print()
-
-    print("== DIGITAL INPUTS ==")
-    print()
-
-    print("== ANALOG INPUTS ==")
-    res = client.read_input_registers(address=0, count=1, device_id=controller_device_id)
-    if res.registers[0] == 0xFFFF:
-        print("Temperature Outdoor Error!")
-    else:
-        print("Temperature Outdoor: {} °C".format(round(check_negative_temperature(res.registers[0]), 1)))
-    res = client.read_input_registers(address=1, count=1, device_id=controller_device_id)
-    if res.registers[0] == 0xFFFF:
-        print("Temperature Indoor Error!")
-    else:
-        print("Temperature Indoor: {} °C".format(round(check_negative_temperature(res.registers[0]), 1)))
-    res = client.read_input_registers(address=400, count=1, device_id=controller_device_id)
-    if res.registers[0] == 0xFFFF:
-        print("Humidity Outdoor Error!")
-    else:
-        print("Humidity Outdoor:    {} %".format(res.registers[0] / 10))
-    res = client.read_input_registers(address=401, count=1, device_id=controller_device_id)
-    if res.registers[0] == 0xFFFF:
-        print("Humidity Indoor Error!")
-    else:
-        print("Humidity Indoor:    {} %".format(res.registers[0] / 10))
-    print()
-
-    print("== ANALOG OUTPUTS (controller information) ==")
-    res = client.read_holding_registers(address=0, count=1, device_id=controller_device_id)
-    print("Device address: {}".format(res.registers[0]))
-    res = client.read_holding_registers(address=2006, count=2, device_id=controller_device_id)
-    print("Firmware Version: {}{}{}{}".format(
-        chr((res.registers[0] >> 8) & 0xFF),
-        chr(res.registers[0] & 0xFF),
-        chr((res.registers[1] >> 8) & 0xFF),
-        chr(res.registers[1] & 0xFF)
-    ))
+    for i in range(1, devices_count + 1):
+        read_device_registers(client, i)
 
 def toggle_coil(client, coil_number):
     res = client.read_coils(address=0, count=2, device_id=controller_device_id)
+    print(res)
     res = client.write_coil(address=coil_number, value=(not res.bits[coil_number]), device_id=controller_device_id)
+    print(res)
+    read_all_devices(client)
+
+def find_device(client):
+    for i in range(1, devices_count + 1):
+        try:
+            res = client.read_holding_registers(address=0, count=1, device_id=i)
+            if res:
+                print(i)
+                return i
+        except BaseException:
+            pass
+
+def change_address(client):
+    print("Enter new address: ", end='')
+    new_address = int(input())
+    if 0 < new_address < 9:
+        res = client.write_register(address=0, value=new_address, device_id=find_device(client))
+        print(res)
 
 def main():
-    client = ModbusSerialClient('/dev/ttyUSB0', baudrate=115200, parity='N', stopbits=1, bytesize=8, framer=FramerType.RTU)
+    client = ModbusSerialClient(tty_port, baudrate=115200, parity='N', stopbits=1, bytesize=8, framer=FramerType.RTU)
     if client.connect():
-        print("connection opened")
+        print("connection opened at " + tty_port)
         while (1):
             print("\navailable actions:")
             print("\t r:  read all registers")
@@ -87,13 +120,15 @@ def main():
             user_select = input()
             match user_select:
                 case 'r':
-                    read_all_registers(client)
+                    read_all_devices(client)
                 case 'q':
                     break
                 case 'c1':
-                    toggle_coil(client, 0);
+                    toggle_coil(client, 0)
                 case 'c2':
-                    toggle_coil(client, 1);
+                    toggle_coil(client, 1)
+                case 'a':
+                    change_address(client)
         client.close()
         print("connection closed")
     else:
